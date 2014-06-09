9ec91a7eada3c941650c117f4cd2e30b92bd893a
toffiak <toffiak@gmail.com>
2014-06-09 09:06:46 +0200

    Added:   Test for lower() and add()

--- tests/Sebbla/Test/Str/StrTest.php
+++ tests/Sebbla/Test/Str/StrTest.php
@@ -1,14 +1,29 @@
 <?php
 
-
 namespace Sebbla\Test\Str;
 
 use Sebbla\Str\Str;
 
 class StrTest extends \PHPUnit_Framework_TestCase
 {
-    public function testUpper(){
-        $s = new Str('mała gęsiareczka na wypasie');
-        $this->assertEquals('MAŁA GĘSIARECZKA NA WYPASIE', $s->upper());
+
+    public function testUpper()
+    {
+        $s = new Str('ęóąśłżźćń');
+        $this->assertEquals('ĘÓĄŚŁŻŹĆŃ', $s->upper());
     }
+
+    public function testLower()
+    {
+        $s = new Str('ĘÓĄŚŁŻŹĆŃ');
+        $this->assertEquals('ęóąśłżźćń', $s->lower());
+    }
+
+    public function testAdd()
+    {
+        $s1 = new Str('Józef');
+        $s2 = new Str(' Piłsudski');
+        $this->assertEquals('Józef Piłsudski', $s1->add($s2));
+    }
+
 }
